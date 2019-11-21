c83e69acc7847a303a03d7b3ad910222ab195a0b
Ian Johnson <ian.johnson@canonical.com>
2019-11-21 15:37:42 -0600

    main.go: implement running multiple iterations
    Move the --number-iterations option, which was previously unimplemented, from
    the run command to the program as a global option. Also rename it to
    additional-iterations so it's clearer when using it in the code how many times
    to execute.
    
    Also add short form of --json option as -j.
    
    Signed-off-by: Ian Johnson <ian.johnson@canonical.com>

--- main.go
+++ main.go
@@ -22,8 +22,9 @@ const sysctlBase = "/proc/sys"
 
 // Command is the command for the runner
 type Command struct {
-	Run        cmdRun `command:"run" description:"Run a command"`
-	ShowErrors bool   `short:"e" long:"errors" description:"Show errors as they happen"`
+	Run                  cmdRun `command:"run" description:"Run a command"`
+	ShowErrors           bool   `short:"e" long:"errors" description:"Show errors as they happen"`
+	AdditionalIterations uint   `short:"n" long:"additional-iterations" description:"Number of additional iterations to run (1 iteration is always run)"`
 }
 
 // OutputResult is the result of running a command with various information
@@ -44,14 +45,13 @@ type cmdRun struct {
 	WindowName       string `short:"w" long:"window-name" description:"Window name to wait for"`
 	PrepareScript    string `short:"p" long:"prepare-script" description:"Script to run to prepare a run"`
 	RestoreScript    string `short:"r" long:"restore-script" description:"Script to run to restore after a run"`
-	Iterations       string `short:"n" long:"number-iterations" description:"Number of iterations to run"`
 	WindowClass      string `short:"c" long:"class-name" description:"Window class to use with xdotool instead of the the first Command"`
 	NoTrace          bool   `short:"t" long:"no-trace" description:"Don't trace the process, just time the total execution"`
 	RunThroughSnap   bool   `short:"s" long:"use-snap-run" description:"Run command through snap run"`
 	DiscardSnapNs    bool   `short:"d" long:"discard-snap-ns" description:"Discard the snap namespace before running the snap"`
 	ProgramStdoutLog string `long:"cmd-stdout" description:"Log file for run command's stdout"`
 	ProgramStderrLog string `long:"cmd-stderr" description:"Log file for run command's stderr"`
-	JSONOutput       bool   `long:"json" description:"Output results in JSON"`
+	JSONOutput       bool   `short:"j" long:"json" description:"Output results in JSON"`
 
 	Args struct {
 		Cmd []string `description:"Command to run" required:"yes"`
@@ -151,6 +151,10 @@ func wmctrlCloseWindow(name string) error {
 
 var errs []error
 
+func resetErrors() {
+	errs = nil
+}
+
 func logError(err error) {
 	errs = append(errs, err)
 	if currentCmd.ShowErrors {
@@ -159,231 +163,245 @@ func logError(err error) {
 }
 
 func (x *cmdRun) Execute(args []string) error {
-	// run the prepare script if it's available
-	if x.PrepareScript != "" {
-		err := runScript(x.PrepareScript)
-		if err != nil {
-			logError(fmt.Errorf("running prepare script: %w", err))
-		}
-	}
-
-	// handle if the command should be run through `snap run`
-	targetCmd := x.Args.Cmd
-	if x.RunThroughSnap {
-		targetCmd = append([]string{"snap", "run"}, targetCmd...)
-	}
-
-	doneCh := make(chan bool, 1)
-	var straceErr error
-	var slg *ExecveTiming
-	var cmd *exec.Cmd
-	var fw *os.File
-	if !x.NoTrace {
-		// setup private tmp dir with strace fifo
-		straceTmp, err := ioutil.TempDir("", "exec-trace")
-		if err != nil {
-			return err
-		}
-		defer os.RemoveAll(straceTmp)
-		straceLog := filepath.Join(straceTmp, "strace.fifo")
-		if err := syscall.Mkfifo(straceLog, 0640); err != nil {
-			return err
-		}
-		// ensure we have one writer on the fifo so that if strace fails
-		// nothing blocks
-		fw, err = os.OpenFile(straceLog, os.O_RDWR, 0640)
-		if err != nil {
-			return err
-		}
-		defer fw.Close()
-
-		// read strace data from fifo async
-		go func() {
-			slg, straceErr = TraceExecveTimings(straceLog, -1)
-			close(doneCh)
-		}()
-
-		cmd, err = TraceExecCommand(straceLog, targetCmd...)
-		if err != nil {
-			return err
-		}
-	} else {
-		// Don't setup tracing, so just use exec.Command directly
-		// x.Args.Cmd (and thus targetCmd) is guaranteed to be at least one
-		// element given that it is a required argument
-		prog := targetCmd[0]
-		var args []string
-		// setup args if there's more than 1
-		if len(targetCmd) > 1 {
-			args = targetCmd[1:]
-		}
-		cmd = exec.Command(prog, args...)
-	}
-
-	cmd.Stdin = os.Stdin
-	// redirect all output from the child process to the log files if they exist
-	// otherwise just to this process's stdout, etc.
-
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	if x.ProgramStdoutLog != "" {
-		f, err := ensureFileExistsAndOpen(x.ProgramStdoutLog)
-		if err != nil {
-			return err
-		}
-		defer f.Close()
-		cmd.Stdout = f
-	}
-	if x.ProgramStderrLog != "" {
-		f, err := ensureFileExistsAndOpen(x.ProgramStderrLog)
-		if err != nil {
-			return err
-		}
-		defer f.Close()
-		cmd.Stderr = f
-	}
-
-	if x.DiscardSnapNs {
-		if !x.RunThroughSnap {
-			return errors.New("cannot use --discard-snap-ns without --use-snap-run")
-		}
-		// the name of the snap in this case is the first argument
-		err := discardSnapNs(x.Args.Cmd[0])
-		if err != nil {
-			return err
-		}
-	}
-
-	// before running the final command, free the caches to get most accurate
-	// timing
-	err := freeCaches()
-	if err != nil {
-		return err
-	}
-
-	// start running the command
-	start := time.Now()
-	err = cmd.Start()
-
-	// now wait until the window appears
-	// err = waitForWindowStateChangeWmctrl(x.WindowName, true)
-	tryXToolClose := true
-	tryWmctrl := false
-
-	windowspec := window{}
-	// check which opts are defined
-	if x.WindowClass != "" {
-		// prefer window class from option
-		windowspec.class = x.WindowClass
-	} else if x.WindowName != "" {
-		// then window name
-		windowspec.name = x.WindowName
-	} else {
-		// finally fall back to base cmd as the class
-		// note we use the original command and note the processed targetCmd
-		// because for example when measuring a snap, we invoke etrace like so:
-		// $ ./etrace run --use-snap chromium
-		// where targetCmd becomes []string{"snap","run","chromium"}
-		// but we still want to use "chromium" as the windowspec class
-		windowspec.class = filepath.Base(x.Args.Cmd[0])
-	}
-
-	xtool := makeXDoTool()
-	wids, err := xtool.waitForWindow(windowspec)
-	if err != nil {
-		logError(fmt.Errorf("waiting for window appearance: %w", err))
-		// if we don't get the wid properly then we can't try closing
-		tryXToolClose = false
-	}
-
-	// save the startup time
-	startup := time.Since(start)
-
-	// now get the pids before closing the window so we can gracefully try
-	// closing the windows before forcibly killing them later
-	if tryXToolClose {
-		pids := make([]int, len(wids))
-		for i, wid := range wids {
-			pid, err := xtool.pidForWindowID(wid)
+	out := OutputResult{}
+	i := uint(0)
+	for i = 0; i < 1+currentCmd.AdditionalIterations; i++ {
+		// run the prepare script if it's available
+		if x.PrepareScript != "" {
+			err := runScript(x.PrepareScript)
 			if err != nil {
-				logError(fmt.Errorf("getting pid for wid %s: %w", wid, err))
-				tryWmctrl = true
-				break
-			}
-			pids[i] = pid
-		}
-
-		// close the windows
-		for _, wid := range wids {
-			err = xtool.closeWindowID(wid)
-			if err != nil {
-				logError(fmt.Errorf("closing window: %w", err))
-				tryWmctrl = true
+				logError(fmt.Errorf("running prepare script: %w", err))
 			}
 		}
 
-		// kill the app pids in case x fails to close the window
-		for _, pid := range pids {
-			// FindProcess always succeeds on unix
-			proc, _ := os.FindProcess(pid)
-			if err := proc.Signal(os.Kill); err != nil {
-				// if the process already exited then try wmctrl
-				if !strings.Contains(err.Error(), "process already finished") {
-					logError(fmt.Errorf("killing window process pid %d: %w", pid, err))
+		// handle if the command should be run through `snap run`
+		targetCmd := x.Args.Cmd
+		if x.RunThroughSnap {
+			targetCmd = append([]string{"snap", "run"}, targetCmd...)
+		}
+
+		doneCh := make(chan bool, 1)
+		var straceErr error
+		var slg *ExecveTiming
+		var cmd *exec.Cmd
+		var fw *os.File
+		if !x.NoTrace {
+			// setup private tmp dir with strace fifo
+			straceTmp, err := ioutil.TempDir("", "exec-trace")
+			if err != nil {
+				return err
+			}
+			defer os.RemoveAll(straceTmp)
+			straceLog := filepath.Join(straceTmp, "strace.fifo")
+			if err := syscall.Mkfifo(straceLog, 0640); err != nil {
+				return err
+			}
+			// ensure we have one writer on the fifo so that if strace fails
+			// nothing blocks
+			fw, err = os.OpenFile(straceLog, os.O_RDWR, 0640)
+			if err != nil {
+				return err
+			}
+			defer fw.Close()
+
+			// read strace data from fifo async
+			go func() {
+				slg, straceErr = TraceExecveTimings(straceLog, -1)
+				close(doneCh)
+			}()
+
+			cmd, err = TraceExecCommand(straceLog, targetCmd...)
+			if err != nil {
+				return err
+			}
+		} else {
+			// Don't setup tracing, so just use exec.Command directly
+			// x.Args.Cmd (and thus targetCmd) is guaranteed to be at least one
+			// element given that it is a required argument
+			prog := targetCmd[0]
+			var args []string
+			// setup args if there's more than 1
+			if len(targetCmd) > 1 {
+				args = targetCmd[1:]
+			}
+			cmd = exec.Command(prog, args...)
+		}
+
+		cmd.Stdin = os.Stdin
+		// redirect all output from the child process to the log files if they exist
+		// otherwise just to this process's stdout, etc.
+
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		if x.ProgramStdoutLog != "" {
+			f, err := ensureFileExistsAndOpen(x.ProgramStdoutLog)
+			if err != nil {
+				return err
+			}
+			defer f.Close()
+			cmd.Stdout = f
+		}
+		if x.ProgramStderrLog != "" {
+			f, err := ensureFileExistsAndOpen(x.ProgramStderrLog)
+			if err != nil {
+				return err
+			}
+			defer f.Close()
+			cmd.Stderr = f
+		}
+
+		if x.DiscardSnapNs {
+			if !x.RunThroughSnap {
+				return errors.New("cannot use --discard-snap-ns without --use-snap-run")
+			}
+			// the name of the snap in this case is the first argument
+			err := discardSnapNs(x.Args.Cmd[0])
+			if err != nil {
+				return err
+			}
+		}
+
+		xtool := makeXDoTool()
+
+		// err = waitForWindowStateChangeWmctrl(x.WindowName, true)
+		tryXToolClose := true
+		tryWmctrl := false
+
+		windowspec := window{}
+		// check which opts are defined
+		if x.WindowClass != "" {
+			// prefer window class from option
+			windowspec.class = x.WindowClass
+		} else if x.WindowName != "" {
+			// then window name
+			windowspec.name = x.WindowName
+		} else {
+			// finally fall back to base cmd as the class
+			// note we use the original command and note the processed targetCmd
+			// because for example when measuring a snap, we invoke etrace like so:
+			// $ ./etrace run --use-snap chromium
+			// where targetCmd becomes []string{"snap","run","chromium"}
+			// but we still want to use "chromium" as the windowspec class
+			windowspec.class = filepath.Base(x.Args.Cmd[0])
+		}
+
+		// before running the final command, free the caches to get most accurate
+		// timing
+		err := freeCaches()
+		if err != nil {
+			return err
+		}
+
+		// start running the command
+		start := time.Now()
+		err = cmd.Start()
+
+		// now wait until the window appears
+
+		wids, err := xtool.waitForWindow(windowspec)
+		if err != nil {
+			logError(fmt.Errorf("waiting for window appearance: %w", err))
+			// if we don't get the wid properly then we can't try closing
+			tryXToolClose = false
+		}
+
+		// save the startup time
+		startup := time.Since(start)
+
+		// now get the pids before closing the window so we can gracefully try
+		// closing the windows before forcibly killing them later
+		if tryXToolClose {
+			pids := make([]int, len(wids))
+			for i, wid := range wids {
+				pid, err := xtool.pidForWindowID(wid)
+				if err != nil {
+					logError(fmt.Errorf("getting pid for wid %s: %w", wid, err))
+					tryWmctrl = true
+					break
+				}
+				pids[i] = pid
+			}
+
+			// close the windows
+			for _, wid := range wids {
+				err = xtool.closeWindowID(wid)
+				if err != nil {
+					logError(fmt.Errorf("closing window: %w", err))
 					tryWmctrl = true
 				}
 			}
-		}
-	}
 
-	if tryWmctrl {
-		err = wmctrlCloseWindow(x.WindowName)
-		if err != nil {
-			logError(fmt.Errorf("closing window with wmctrl: %w", err))
-		}
-	}
-
-	if !x.NoTrace {
-		// ensure we close the fifo here so that the strace.TraceExecCommand()
-		// helper gets a EOF from the fifo (i.e. all writers must be closed
-		// for this)
-		fw.Close()
-
-		// wait for strace reader
-		<-doneCh
-		if straceErr == nil {
-			// make a new tabwriter to stderr
-			if !x.JSONOutput {
-				w := tabWriterGeneric(os.Stderr)
-				slg.Display(w)
+			// kill the app pids in case x fails to close the window
+			for _, pid := range pids {
+				// FindProcess always succeeds on unix
+				proc, _ := os.FindProcess(pid)
+				if err := proc.Signal(os.Kill); err != nil {
+					// if the process already exited then try wmctrl
+					if !strings.Contains(err.Error(), "process already finished") {
+						logError(fmt.Errorf("killing window process pid %d: %w", pid, err))
+						tryWmctrl = true
+					}
+				}
 			}
+		}
+
+		if tryWmctrl {
+			err = wmctrlCloseWindow(x.WindowName)
+			if err != nil {
+				logError(fmt.Errorf("closing window with wmctrl: %w", err))
+			}
+		}
+
+		if !x.NoTrace {
+			// ensure we close the fifo here so that the strace.TraceExecCommand()
+			// helper gets a EOF from the fifo (i.e. all writers must be closed
+			// for this)
+			fw.Close()
+
+			// wait for strace reader
+			<-doneCh
+			if straceErr == nil {
+				// make a new tabwriter to stderr
+				if !x.JSONOutput {
+					w := tabWriterGeneric(os.Stderr)
+					slg.Display(w)
+				}
+			} else {
+				logError(fmt.Errorf("cannot extract runtime data: %w", straceErr))
+			}
+		}
+
+		if x.RestoreScript != "" {
+			err := runScript(x.RestoreScript)
+			if err != nil {
+				logError(fmt.Errorf("running restore script: %w", err))
+			}
+		}
+
+		run := Execution{
+			ExecveTiming:  slg,
+			TimeToDisplay: startup,
+			Errors:        errs,
+		}
+
+		// if we're not tracing then just use startup time as time to run
+		if x.NoTrace {
+			run.TimeToRun = startup
 		} else {
-			logError(fmt.Errorf("cannot extract runtime data: %w", straceErr))
+			run.TimeToRun = slg.TotalTime
 		}
-	}
 
-	if x.RestoreScript != "" {
-		err := runScript(x.RestoreScript)
-		if err != nil {
-			logError(fmt.Errorf("running restore script: %w", err))
+		out.Runs = append(out.Runs, run)
+
+		if !x.JSONOutput {
+			fmt.Println("Total startup time:", startup)
 		}
-	}
 
-	out := OutputResult{
-		Runs: []Execution{
-			{
-				ExecveTiming:  slg,
-				TimeToDisplay: startup,
-				TimeToRun:     slg.TotalTime,
-				Errors:        errs,
-			},
-		},
+		resetErrors()
 	}
 
 	if x.JSONOutput {
 		json.NewEncoder(os.Stdout).Encode(out)
-	} else {
-		fmt.Println("Total startup time:", startup)
 	}
 
 	return nil
