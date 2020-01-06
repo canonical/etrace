bc615877e6bb33f9873e4ce244e81f3c5c83e135
Ian Johnson <person.uwsome@gmail.com>
2020-01-05 19:58:31 -0600

    [wip] remove content snaps during prepare script
    Signed-off-by: Ian Johnson <person.uwsome@gmail.com>

--- prepare-snap.sh
+++ prepare-snap.sh
@@ -11,11 +11,37 @@ if [ "$1" = "chromium" ]; then
     rm -rf ~/.config/chromium
 fi
 
+# if the snap is using the gnome content snaps, remove those and reinstall them
+# so that we don't end up caching those snaps as well
+if [ "$1" = "libreoffice" ]; then
+    snap remove gnome-3-28-1804 gtk-common-themes
+
+    # for snap-repository versions
+    snap install --dangerous /home/ijohnson/git/etrace/snap-repository/no-compression/gnome-3-28-1804.snap
+    snap install --dangerous /home/ijohnson/git/etrace/snap-repository/no-compression/gtk-common-themes.snap
+
+    # for vanilla versions
+    # snap install gnome-3-28-1804 gtk-common-themes
+fi
+
 # install our local copy of the snap rev to insulate against upgrades to the
 # stable channel
 sudo snap install --dangerous "$2"
 
+# try connecting finnicky interfaces which require a slot to be specified if 
+# they are plugged by the snap
+for iface in icon-themes gtk-3-themes sound-themes; do
+    if snap connections "$1" | grep -q $iface; then
+        snap connect "$1:$iface" "gtk-common-themes:$iface"
+    fi
+done
+
+if snap connections "$1" | grep -q "gnome-3-28-1804"; then
+    snap connect "$1:gnome-3-28-1804" gnome-3-28-1804:gnome-3-28-1804
+fi
+
 # connect all interfaces
+# TODO: update to use `snap connections` instead
 for iface in $(snap interfaces "$1" 2>/dev/null | grep -P "^-" | awk '{print $2}'); 
     do sudo snap connect "$iface" >&2
 done
