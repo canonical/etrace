4e5a5eff61b9eeca1cc8361934f5a1bb9ccd465d
Ian Johnson <person.uwsome@gmail.com>
2020-07-14 20:19:15 -0500

    internal/snaps: add CurrentConnections and ApplyConnection
    This will make it easier to work on multiple snaps when we i.e. handle
    reinstalling content snaps too.
    
    Signed-off-by: Ian Johnson <person.uwsome@gmail.com>

--- internal/snaps/snaps.go
+++ internal/snaps/snaps.go
@@ -18,10 +18,13 @@
 package snaps
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 
 	"github.com/anonymouse64/etrace/internal/commands"
 )
@@ -52,4 +55,92 @@ func Revision(snap string) (string, error) {
 	}
 	// get the revision by reading the "current" link in /snap/$SNAP_NAME
 	return os.Readlink(filepath.Join(snapDir, "current"))
+}
+
+// Connection represents an interface connection between two snaps.
+type Connection struct {
+	Plug      string
+	PlugSnap  string
+	Slot      string
+	SlotSnap  string
+	Interface string
+}
+
+// ApplyConnection runs snap connect to make the specified connection.
+func ApplyConnection(conn Connection) error {
+	plug := conn.PlugSnap + ":" + conn.Plug
+	slot := conn.SlotSnap + ":" + conn.Slot
+	connectCmd := exec.Command("snap", "connect", plug, slot)
+	err := commands.AddSudoIfNeeded(connectCmd)
+	if err != nil {
+		return fmt.Errorf("failed to add sudo to command: %v", err)
+	}
+	connectOut, err := connectCmd.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("failed to apply connection from %s to %s: %v (%s)", plug, slot, err, string(connectOut))
+	}
+	return nil
+}
+
+// CurrentConnections returns the connections of the snap.
+func CurrentConnections(snapName string) ([]Connection, error) {
+	// save interface connections
+	ifacesOut, err := exec.Command("snap", "connections", snapName).CombinedOutput()
+	if err != nil {
+		return nil, fmt.Errorf("failed to save snap connections output: %v (%s)", err, string(ifacesOut))
+	}
+
+	s := bufio.NewScanner(bytes.NewReader(ifacesOut))
+
+	var conns []Connection
+
+	// discard the first line as that's the column headers
+	// but also if a snap has no connections then the output here will
+	// be empty, so we just continue as normal here
+	if s.Scan() {
+		// empty output somehow
+		for s.Scan() {
+			// split each connection by whitespace
+			fields := strings.Fields(s.Text())
+
+			if len(fields) != 4 {
+				return nil, fmt.Errorf("error saving interface state: unexpected number of rows from snap connections output")
+			}
+
+			// ignore disconnected plugs and slots which are indicated
+			// by "-" in the snap connections output
+			if fields[1] == "-" || fields[2] == "-" {
+				continue
+			}
+			// the first column is the interface type which we don't care
+			// about
+			// the second column is the plug, which we do care about
+			// the third column is the slot, which we also care about
+
+			plugFields := strings.SplitN(fields[1], ":", 2)
+			if len(plugFields) != 2 {
+				return nil, fmt.Errorf("unexpected snap connections output format, expected plug to have \":\" in it")
+			}
+			if plugFields[0] == "" {
+				plugFields[0] = "system"
+			}
+			slotFields := strings.SplitN(fields[2], ":", 2)
+			if len(slotFields) != 2 {
+				return nil, fmt.Errorf("unexpected snap connections output format, expected plug to have \":\" in it")
+			}
+			if slotFields[0] == "" {
+				slotFields[0] = "system"
+			}
+			conn := Connection{
+				Interface: fields[0],
+				PlugSnap:  plugFields[0],
+				Plug:      plugFields[1],
+				SlotSnap:  slotFields[0],
+				Slot:      slotFields[1],
+			}
+			conns = append(conns, conn)
+		}
+	}
+
+	return conns, nil
 }
